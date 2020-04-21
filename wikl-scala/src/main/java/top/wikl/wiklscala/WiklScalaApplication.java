f2d6ece145e92df25c5b166a7f31182f3f33e033
夏亚岭 <only_xyl@163.com>
2020-04-21 19:41:23 +0800

    WiklGraph 1. 修改 【wikl-scala】 模块

--- wikl-scala/src/main/java/top/wikl/wiklscala/WiklScalaApplication.java
+++ wikl-scala/src/main/java/top/wikl/wiklscala/WiklScalaApplication.java
@@ -3,11 +3,17 @@ package top.wikl.wiklscala;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author XYL
+ * @title: WiklScalaApplication
+ * @description: TODO
+ * @date 2020/4/21 16:48
+ * @return
+ * @since V1.0
+ */
 @SpringBootApplication
 public class WiklScalaApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(WiklScalaApplication.class, args);
     }
-
 }
